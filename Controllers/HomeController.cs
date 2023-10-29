776c58554a5f7d581b53048671bf082b5af7c9de
NishitAmin <nishit964@gmail.com>
2023-10-28 23:09:58 -0400

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mix_Master.Models;
+
+namespace Mix_Master.Controllers
+{
+    public class HomeController : Controller
+    {
+        MixMasterEntities db = new MixMasterEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.TBLUserInfoes.ToList());
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Products()
+        {
+            return View();
+        }
+        public ActionResult Store()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(TBLUserInfo tBLUserInfo)
+        {
+            if(db.TBLUserInfoes.Any(x => x.UsernameUs == tBLUserInfo.UsernameUs))
+            {
+                ViewBag.Notification = "Account already exists";
+                return View();
+            }
+            else
+            {
+                try
+                {
+                    db.TBLUserInfoes.Add(tBLUserInfo);
+                    db.SaveChanges();
+
+                    Session["IdUsSS"] = tBLUserInfo.IdUs.ToString();
+                    Session["UsernameSS"] = tBLUserInfo.UsernameUs.ToString();
+                    return RedirectToAction("Index", "Home");
+                }
+                catch (DbEntityValidationException e)
+                {
+                    foreach (var eve in e.EntityValidationErrors)
+                    {
+                        Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                            eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                        foreach (var ve in eve.ValidationErrors)
+                        {
+                            Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                                ve.PropertyName, ve.ErrorMessage);
+                        }
+                    }
+                    throw;
+                }
+                
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(TBLUserInfo tBLUserInfo)
+        {
+            var checkLogin = db.TBLUserInfoes.Where(x => x.UsernameUs.Equals(tBLUserInfo.UsernameUs) && x.PasswordUs.Equals(tBLUserInfo.PasswordUs)).FirstOrDefault();
+            if(checkLogin != null)
+            {
+                Session["IdUsSS"] = tBLUserInfo.IdUs.ToString();
+                Session["UsernameSS"] = tBLUserInfo.UsernameUs.ToString();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Notification = "Wrond Username Or Password";
+            }
+            return View();
+        }
+    }
+}
