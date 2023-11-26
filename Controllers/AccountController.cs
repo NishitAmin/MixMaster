38f0f6401e96e333184d1d6714b2da98ed5093c4
NishitAmin <nishit964@gmail.com>
2023-11-26 15:15:24 -0500

    feature_db

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mix_Master.Models;
+
+namespace Mix_Master.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
